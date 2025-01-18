1000452f2add6bc974532570e259a2e2a1b38cf7
Shreya Nayak <shryanayak97@gmail.com>
2025-01-18 13:59:19 -0600

    Update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,14 @@ const Navbar = () => {
         justify-center flex font-bold shadow-md'>
             <p className='blue-gradient_text'>SN</p>
         </NavLink>
+        
+        {/* Center text */}
+        <div className='flex-1 text-center'>
+          <p className='text-lg font-medium text-gray-700 italic'>
+            This website is under construction
+          </p>
+        </div>
+
         <nav className='flex text-lg gap-7 font-medium'>
           <NavLink to='/about' className={({ isActive }) => isActive ?
           'text-blue-500' : 'text-black'}>
