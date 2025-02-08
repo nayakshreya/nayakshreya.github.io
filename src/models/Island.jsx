7d512d04e53ea59cb42595578c6fe2a1ca4a268d
Shreya Nayak <shryanayak97@gmail.com>
2025-02-08 01:54:40 -0600

    Add Projects page

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -67,9 +67,11 @@ export default function Island({
       if(e.key === 'ArrowLeft') {
         if(!isRotating) setIsRotating(true);
         islandRef.current.rotation.y += 0.01 * Math.PI;
+        rotationSpeed.current = 0.0125;
       } else if(e.key === 'ArrowRight') {
         if(!isRotating) setIsRotating(true);
         islandRef.current.rotation.y -= 0.01 * Math.PI;
+        rotationSpeed.current = -0.0125;
       }
     }
 
